ca2b94a1b111478ebc7801c1a402b855f1f18755
Robert Anderson <robert@noisysocks.com>
2021-04-09 16:18:20 +1000

    Legacy Widget: Fire widget-added and widget-changed events

--- packages/block-library/src/legacy-widget/edit/form.js
+++ packages/block-library/src/legacy-widget/edit/form.js
@@ -19,9 +19,13 @@ import {
 } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { Button } from '@wordpress/components';
+import { useInstanceId } from '@wordpress/compose';
+
+const $ = window.jQuery;
 
 export default function Form( { id, idBase, instance, setInstance } ) {
-	const ref = useRef();
+	const containerRef = useRef();
+	const formRef = useRef();
 
 	const { html, setFormData } = useForm( {
 		id,
@@ -30,11 +34,22 @@ export default function Form( { id, idBase, instance, setInstance } ) {
 		setInstance,
 	} );
 
+	const hasBeenAdded = useRef( false );
+	useEffect( () => {
+		if ( html ) {
+			$( document ).trigger(
+				hasBeenAdded.current ? 'widget-updated' : 'widget-added',
+				[ $( containerRef.current ) ]
+			);
+			hasBeenAdded.current = true;
+		}
+	}, [ html ] );
+
 	const onSubmit = useCallback(
 		( event ) => {
 			event.preventDefault();
 			if ( id ) {
-				setFormData( serializeForm( ref.current ) );
+				setFormData( serializeForm( formRef.current ) );
 			}
 		},
 		[ id ]
@@ -43,22 +58,23 @@ export default function Form( { id, idBase, instance, setInstance } ) {
 	const onChange = useCallback(
 		debounce( () => {
 			if ( idBase ) {
-				setFormData( serializeForm( ref.current ) );
+				setFormData( serializeForm( formRef.current ) );
 			}
 		}, 300 ),
 		[ idBase ]
 	);
 
 	return (
-		<div className="widget open">
+		<div ref={ containerRef } className="widget open">
 			<div className="widget-inside">
 				<ObservableForm
-					ref={ ref }
+					ref={ formRef }
 					className="form"
 					method="post"
 					onSubmit={ onSubmit }
 					onChange={ onChange }
 				>
+					<HiddenInputs id={ id } idBase={ idBase } />
 					<RawHTML className="widget-content">{ html }</RawHTML>
 					{ id && (
 						<Button type="submit" isPrimary>
@@ -166,14 +182,60 @@ const ObservableForm = forwardRef( ( { onChange, ...props }, ref ) => {
 	// around this by not using React's event system.
 
 	useEffect( () => {
-		const handler = () => onChange( ref.current );
-		ref.current.addEventListener( 'change', handler );
-		ref.current.addEventListener( 'input', handler );
+		ref.current.addEventListener( 'change', onChange );
+		ref.current.addEventListener( 'input', onChange );
+		$( ref.current ).on( 'change', null, onChange );
 		return () => {
-			ref.current.removeEventListener( 'change', handler );
-			ref.current.removeEventListener( 'input', handler );
+			ref.current.removeEventListener( 'change', onChange );
+			ref.current.removeEventListener( 'input', onChange );
+			$( ref.current ).off( 'change', null, onChange );
 		};
 	}, [ onChange ] );
 
 	return <form ref={ ref } { ...props } />;
 } );
+
+function HiddenInputs( { id, idBase } ) {
+	// Ensure compatibility with widgets which have scripts that expect these
+	// hidden inputs to exist in the widget's DOM.
+
+	// We don't know the real widget number until the widget is saved, which
+	// might not ever happen. Instead, use a fake unique number.
+	const number = useInstanceId( HiddenInputs );
+
+	return (
+		<>
+			<input
+				type="hidden"
+				name="widget-id"
+				className="widget-id"
+				value={ id ?? `${ idBase }-${ number }` }
+			/>
+			<input
+				type="hidden"
+				name="id_base"
+				className="id_base"
+				value={ idBase ?? id }
+			/>
+			<input
+				type="hidden"
+				name="widget-width"
+				className="widget-width"
+				value="250"
+			/>
+			<input
+				type="hidden"
+				name="widget-height"
+				className="widget-height"
+				value="200"
+			/>
+			<input
+				type="hidden"
+				name="widget_number"
+				className="widget_number"
+				value={ idBase ? number : '' }
+			/>
+			<input type="hidden" name="add_new" className="add_new" value="" />
+		</>
+	);
+}
